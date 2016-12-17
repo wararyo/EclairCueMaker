71351e68900351165fd86672bc55cdf6433893af
wararyo <wararyo@gmail.com>
2016-12-17 19:54:34 +0900

    目盛り
    Timeline上部の目盛りを作った

---
+++ EclairEventer/Assets/EclairEventer/Editor/EclairGUILayout.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace wararyo.EclairEventer
+{
+
+    public class EclairGUILayout
+    {
+
+        public static void Ruler (float startTime,float endTime,params GUILayoutOption[] options)
+        {
+            using (new EditorGUI.DisabledScope(true)) GUILayout.Button("", EditorStyles.toolbarButton,options);
+            //EditorGUILayout.LabelField("", "", EditorStyles.toolbar, options);
+            var rect = GUILayoutUtility.GetLastRect();//既存のGUILayoutを実行しRectだけ盗む
+            EclairGUILayout.Ruler(rect, startTime, endTime);
+        }
+
+        public static void Ruler(float startTime, float endTime)
+        {
+            using (new EditorGUI.DisabledScope(true)) GUILayout.Button("", EditorStyles.toolbarButton);
+            var rect = GUILayoutUtility.GetLastRect();//既存のGUILayoutを実行しRectだけ盗む
+            EclairGUILayout.Ruler(rect, startTime, endTime);
+        }
+
+        public static void Ruler (Rect rect,float startTime, float endTime)
+        {
+            if (rect.height == 1.0f) return;
+
+            var id = GUIUtility.GetControlID(FocusType.Keyboard, rect);
+
+            Texture2D tex = new Texture2D((int)rect.width, (int)rect.height);
+            tex.alphaIsTransparency = true;
+            //tex.filterMode = FilterMode.Point;
+
+            Color[] transparent = new Color[tex.width * tex.height];
+            for (int i = 0;i < transparent.Length;i++)
+            {
+                transparent[i] = new Color(0, 0, 0, 0);
+            }
+            tex.SetPixels(0, 0, tex.width, tex.height,transparent);
+
+            //目盛りと数字の描画開始
+            for(float i=((int)(startTime*10))/(float)10;i < endTime; i += 0.1f)//目盛り単位0.1秒
+            {
+                i = (int)(i * 10 + 0.5f) / (float)10;
+                if (i < startTime) continue;
+                int x = (int)Mathf.Lerp(0, rect.width, (i - startTime) / (endTime - startTime));
+                int height = 4;
+                if (((int)(i * 10 + 0.5f)) % 10 == 0)
+                {
+                    height = 12;
+                    GUI.Label(new Rect(rect.x + x + 2, rect.y - 2, 32, 16), i + "");
+                }
+                else if ((int)(i * 10 + 0.5f) % 5 == 0) height = 6;
+                tex = drawScale(tex, x, height, new Color(0, 0, 0, 1));
+
+            }
+            tex.Apply();
+            GUI.DrawTexture(rect, tex);
+        }
+
+        /// <summary>
+        /// Texture2Dに目盛りを書きます。
+        /// </summary>
+        /// <param name="tex"></param>
+        /// <param name="x"></param>
+        /// <param name="height"></param>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        static Texture2D drawScale(Texture2D tex,int x,int height,Color color)
+        {
+            for (int i = 0; i < height; i++) {
+                tex.SetPixel(x, i, color);
+            }
+            return tex;
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
--- EclairEventer/Assets/EclairEventer/Editor/EventEditor.cs
+++ EclairEventer/Assets/EclairEventer/Editor/EventEditor.cs
@@ -7,6 +7,7 @@ namespace wararyo.EclairEventer
     public class EventEditor : EditorWindow
     {
         private int tab = 0;
+        private int paneWidth = 256;//timelineの左側の幅
 
         [MenuItem("Window/EclairEventEditor")]
         static void Open()
@@ -20,17 +21,29 @@ namespace wararyo.EclairEventer
             {
                 tab = GUILayout.Toolbar(tab, new string[] { "Timeline", "Raw" }, EditorStyles.toolbarButton, GUILayout.Width(180));
                 //GUILayout.Space(20);
-                using (new EditorGUI.DisabledScope(true)) GUILayout.Button("", EditorStyles.toolbarButton);
+                toolbarSpace(16);//こうしないとRawボタンの右側の線が表示されない
+                using (new EditorGUI.DisabledScope(true)) GUILayout.Button("Play", EditorStyles.toolbarButton,GUILayout.Width(64));
+                EditorGUILayout.Space();
             }
             if (tab == 0)//Timelineタブ
             {
-                // 試しにラベルを表示
-                EditorGUILayout.LabelField("ようこそ！　Unityエディタ拡張の沼へ！");
+                using (var rulerScope = new EditorGUILayout.HorizontalScope("toolbar"))
+                {
+                    GUILayout.Button("Add", EditorStyles.toolbarButton, GUILayout.Width(64));
+                    toolbarSpace(paneWidth - 64 - 6);
+                    //ここまで256px
+                    EclairGUILayout.Ruler(3.95f, 5.55f);
+                }
             }
             else//Rawタブ
             {
                 EditorGUILayout.LabelField("こちらはRawタブとなっております。");
             }
+        }
+
+        private void toolbarSpace(int width = 6)
+        {
+            using (new EditorGUI.DisabledScope(true)) GUILayout.Button("", EditorStyles.toolbarButton, GUILayout.Width(width));
         }
     }
 }
