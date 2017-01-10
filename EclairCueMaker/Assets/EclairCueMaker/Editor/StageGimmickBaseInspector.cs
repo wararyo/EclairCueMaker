c119311dd7e9ffdd076c7062be5de1eb615fc5cb
wararyo <wararyo@gmail.com>
2017-01-11 00:14:05 +0900

    StageGimmickBaseInspector

---
+++ EclairCueMaker/Assets/EclairCueMaker/Editor/StageGimmickBaseInspector.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+namespace wararyo.EclairCueMaker
+{
+    [CustomEditor(typeof(StageGimmickBase),true)]//このtrueが大事っぽい！！！！こんなニッチな要求を想定してるなんて…Unity最高
+    public class StageGimmickBaseInspector : Editor
+    {
+        SerializedProperty targetProperty, cueEventIDProperty, parameterProerty;
+
+        void OnEnable()
+        {
+            targetProperty = serializedObject.FindProperty("target");
+
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.LabelField("Cue");
+
+            base.OnInspectorGUI();
+        }
+    }
+}
